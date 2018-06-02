a8d6ea6ca14709068c92456622976d5b5cf37c24
lb <libinkuaile@126.com>
2018-06-02 09:42:07 +0800

    pid初始化放在中断使能之前

--- src/main.c
+++ src/main.c
@@ -14,25 +14,8 @@ PID speedPID;
 PID currentPID;
 COMMAND upperCommand;
 Uint16 reciveFlag = 0;
-
 Uint16 moveCnt = 0;
 Uint16 duty = 0;
-Uint16 keepCnt = 0;
-
-void initPID()
-{
-	memset(&speedPID,0x00,sizeof(PID));
-	speedPID.outMax = 196608000;//%80
-	speedPID.outMin = 0;//
-	speedPID.kp = 262144;//4
-	speedPID.ki = 13107;//0.2
-
-//	currentPID.outMax = 100*65536;
-//	currentPID.outMin = 0;
-//	currentPID.kp = 10000;
-//	currentPID.ki = 10;
-
-}
 
 int main()
 {
@@ -40,11 +23,7 @@ int main()
     /*initialize DSP */
 	dsp28335Init();
 
-    /*initialize Controller*/
-	initPID();
-
-    readHall();
-
+	readHall();
     SET_PWM_PERCENT(duty);
 
     while (1) 
@@ -57,17 +36,17 @@ int main()
     		currentPID.mode = (MANUAL_STA == backData.status) ? MANUAL : AUTOMATIC;
     		speedPID.mode = (MANUAL_STA == backData.status) ? MANUAL : AUTOMATIC;
     	 }
-
 #if SPEED
-    	/*speed pid */
+    	/* speed pid */
     	if(0 != speedLoopSample)
     	{
     		speedLoopSample = 0;
-		    if(FOREWARD_STA == backData.status || BACKWARD_STA == backData.status )
+ 	        if(FOREWARD_STA == backData.status || BACKWARD_STA == backData.status )
 		    {
+ 	        	//speedPID.setPoint = 100;
 		    	speedPID.input = backData.speedCapture;
 		    	pidCalc(&speedPID);
-		    	duty = (Uint16)(speedPID.sumOut * 100/3750) ;
+		    	duty = (Uint16)(speedPID.sumOut * 100/3750);
 		    	SET_PWM(3750-speedPID.sumOut);
 		    }
     	}
